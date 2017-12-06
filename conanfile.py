c817feb538bf8a8c11a6f93be47ed19b3aafda0c
Rene Rivera <grafikrobot@gmail.com>
2017-12-06 08:13:48 -0600

    Require generator.

--- conanfile.py
+++ conanfile.py
@@ -4,15 +4,12 @@ from conans import ConanFile, tools
 class BoostRegexConan(ConanFile):
     name = "Boost.Regex"
     version = "1.65.1"
-    url = "https://github.com/bincrafters/conan-boost-regex"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
-    license = "www.boost.org/users/license.html"
 
-    settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "use_icu": [True, False]}
     default_options = "shared=False", "use_icu=False"
 
     requires = \
+        "Boost.Generator/1.65.1@bincrafters/testing", \
         "Boost.Assert/1.65.1@bincrafters/testing", \
         "Boost.Concept_Check/1.65.1@bincrafters/testing", \
         "Boost.Config/1.65.1@bincrafters/testing", \
@@ -36,20 +33,19 @@ class BoostRegexConan(ConanFile):
 
     # BEGIN
 
+    url = "https://github.com/bincrafters/conan-boost-regex"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    license = "www.boost.org/users/license.html"
     short_paths = True
     build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
     generators = "boost"
+    settings = "os", "arch", "compiler", "build_type"
 
-    def package_id(self):
-        if self.is_header_only:
-            self.info.header_only()
-
-    # pylint: disable=unused-import
     @property
     def env(self):
         try:
             with tools.pythonpath(super(self.__class__, self)):
-                import boostgenerator # pylint: disable=F0401
+                import boostgenerator  # pylint: disable=F0401
                 boostgenerator.BoostConanFile(self)
         except:
             pass
